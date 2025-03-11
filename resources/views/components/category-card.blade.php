819f1be6ab15ac1c53f4410f02bddfc9561bef74
Ivan on Linux <admin@codprez.com>
2025-03-11 20:34:24 +0300

    fixed image fetching

--- resources/views/components/category-card.blade.php
+++ resources/views/components/category-card.blade.php
@@ -1,11 +1,11 @@
 <div class="item-slick">
     <div class="post-item">
-        <img src="{{$category->image->url}}" alt="IMG">
+        <img src="{{ $category->thumbnail ? $category->url : '' }}" alt="IMG">
 
         <div class="overlay"></div>
 
         <a href="#" class="content">
-            <span class="title">{{$category->name}}</span> {{ $category->videos_count }} Videos
+            <span class="title">{{ $category->name }}</span> {{ $category->videos_count }} Videos
         </a>
     </div>
 </div>
